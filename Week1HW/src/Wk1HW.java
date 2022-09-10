c98d8a8293ddd4afe434d7d9bbf925752ea264ff
Stella Dobson <stellamdobson@gmail.com>
2022-09-09 21:47:58 -0700

    first commit

---
+++ Week1HW/src/Wk1HW.java
@@ -0,0 +1,17 @@
+
+public class Wk1HW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//playing with java
+System.out.println("This is my test program");
+		
+		String myName = "Stella Dobson";
+		String state = "Oregon";
+		
+		System.out.println("Hello, my name is " + myName);
+	
+
+	}
+
+}
